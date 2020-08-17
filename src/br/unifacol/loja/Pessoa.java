4b85c2a2979b666f93100fe343b58358a2df2bd5
Ricardo <Ricardo@DESKTOP-Q3I09MN>
2020-08-17 20:10:20 -0300

    Criação da classe pessoa; Para classe Vendedor: Inclusão dos modificadores de acesso; Inclusção de Construtores; Modificadores de acesso método e atributos; add Herança e sobrecarga de  construtores e métodos

---
+++ src/br/unifacol/loja/Pessoa.java
@@ -0,0 +1,100 @@
+package br.unifacol.loja;
+
+import java.util.Date;
+
+public abstract class Pessoa {
+	protected String nome; 
+	protected Date dataNascimento;	 
+	protected String enderecoRua;
+	protected String ederecoBairro;
+	protected String enderecoCep;
+	protected String enderecoNumero;
+	protected String enderecoComplemento;
+	protected String telefone;
+	protected String email;
+	protected String whatapp;
+	
+	
+	
+	public Pessoa(String nome, Date dataNascimento, String enderecoRua, String ederecoBairro, String enderecoCep,
+			String enderecoNumero, String enderecoComplemento, String telefone, String email, String whatapp) {
+		super();
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.enderecoRua = enderecoRua;
+		this.ederecoBairro = ederecoBairro;
+		this.enderecoCep = enderecoCep;
+		this.enderecoNumero = enderecoNumero;
+		this.enderecoComplemento = enderecoComplemento;
+		this.telefone = telefone;
+		this.email = email;
+		this.whatapp = whatapp;
+	}
+	
+	public Pessoa() {
+		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getEnderecoRua() {
+		return enderecoRua;
+	}
+	public void setEnderecoRua(String enderecoRua) {
+		this.enderecoRua = enderecoRua;
+	}
+	public String getEderecoBairro() {
+		return ederecoBairro;
+	}
+	public void setEderecoBairro(String ederecoBairro) {
+		this.ederecoBairro = ederecoBairro;
+	}
+	public String getEnderecoCep() {
+		return enderecoCep;
+	}
+	public void setEnderecoCep(String enderecoCep) {
+		this.enderecoCep = enderecoCep;
+	}
+	public String getEnderecoNumero() {
+		return enderecoNumero;
+	}
+	public void setEnderecoNumero(String enderecoNumero) {
+		this.enderecoNumero = enderecoNumero;
+	}
+	public String getEnderecoComplemento() {
+		return enderecoComplemento;
+	}
+	public void setEnderecoComplemento(String enderecoComplemento) {
+		this.enderecoComplemento = enderecoComplemento;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getWhatapp() {
+		return whatapp;
+	}
+	public void setWhatapp(String whatapp) {
+		this.whatapp = whatapp;
+	} 
+	
+	
+}
--- src/br/unifacol/loja/Vendedor.java
+++ src/br/unifacol/loja/Vendedor.java
@@ -2,23 +2,173 @@ package br.unifacol.loja;
 
 import java.util.Date;
 
-public class Vendedor {
-	public String nome;
-	public String matricula;
-	public Date dataAdmissao;
-	public Date dataDelisgamento;
-	public boolean situacao;
-	public float metaVendas;
-	public float VendasAnterior;
-	public float VendasAtual;
-	public int qtdClientesAtendidos;
-	public String enderecoRua;
-	public String ederecoBairro;
-	public String enderecoCep;
-	public String enderecoNumero;
-	public String enderecoComplemento;
-	public String telefone;
-	public String email;
-	public String whatapp;
-	public String nomeLoja;
+public class Vendedor  extends Pessoa{
+	 
+	private String matricula;
+	private Date dataAdmissao;
+	private Date dataDelisgamento;
+	private boolean situacao;
+	private float metaVendas;
+	private float VendasAnterior;
+	private float VendasAtual;
+	private int qtdClientesAtendidos;
+	private String enderecoLojaRua;
+	private String ederecoLojaBairro;
+	private String enderecoLojaCep;
+	private String enderecoLojaNumero;
+	private String enderecoLojaComplemento;
+	private String telefone;
+	private String email;
+	private String whatapp;
+	private String nomeLoja;
+	
+	
+	
+	public Vendedor(String nome, Date dataNascimento, String enderecoRua, String ederecoBairro, String enderecoCep,
+			String enderecoNumero, String enderecoComplemento, String telefone, String email, String whatapp,
+			String matricula, Date dataAdmissao, Date dataDelisgamento, boolean situacao, float metaVendas,
+			float vendasAnterior, float vendasAtual, int qtdClientesAtendidos, String enderecoLojaRua,
+			String ederecoLojaBairro, String enderecoLojaCep, String enderecoLojaNumero, String enderecoLojaComplemento,
+			String telefone2, String email2, String whatapp2, String nomeLoja) {
+		super(nome, dataNascimento, enderecoRua, ederecoBairro, enderecoCep, enderecoNumero, enderecoComplemento,
+				telefone, email, whatapp);
+		this.matricula = matricula;
+		this.dataAdmissao = dataAdmissao;
+		this.dataDelisgamento = dataDelisgamento;
+		this.situacao = situacao;
+		this.metaVendas = metaVendas;
+		VendasAnterior = vendasAnterior;
+		VendasAtual = vendasAtual;
+		this.qtdClientesAtendidos = qtdClientesAtendidos;
+		this.enderecoLojaRua = enderecoLojaRua;
+		this.ederecoLojaBairro = ederecoLojaBairro;
+		this.enderecoLojaCep = enderecoLojaCep;
+		this.enderecoLojaNumero = enderecoLojaNumero;
+		this.enderecoLojaComplemento = enderecoLojaComplemento;
+		telefone = telefone2;
+		email = email2;
+		whatapp = whatapp2;
+		this.nomeLoja = nomeLoja;
+	}
+	
+	
+	
+	public Vendedor(String nome, Date dataNascimento, String enderecoRua, String ederecoBairro, String enderecoCep,
+			String enderecoNumero, String enderecoComplemento, String telefone, String email, String whatapp) {
+		super(nome, dataNascimento, enderecoRua, ederecoBairro, enderecoCep, enderecoNumero, enderecoComplemento,
+				telefone, email, whatapp);
+	}
+
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public Date getDataAdmissao() {
+		return dataAdmissao;
+	}
+	public void setDataAdmissao(Date dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+	public Date getDataDelisgamento() {
+		return dataDelisgamento;
+	}
+	public void setDataDelisgamento(Date dataDelisgamento) {
+		this.dataDelisgamento = dataDelisgamento;
+	}
+	public boolean isSituacao() {
+		return situacao;
+	}
+	public void setSituacao(boolean situacao) {
+		this.situacao = situacao;
+	}
+	public float getMetaVendas() {
+		return metaVendas;
+	}
+	public void setMetaVendas(float metaVendas) {
+		this.metaVendas = metaVendas;
+	}
+	public float getVendasAnterior() {
+		return VendasAnterior;
+	}
+	public void setVendasAnterior(float vendasAnterior) {
+		VendasAnterior = vendasAnterior;
+	}
+	public float getVendasAtual() {
+		return VendasAtual;
+	}
+	public void setVendasAtual(float vendasAtual) {
+		VendasAtual = vendasAtual;
+	}
+	public int getQtdClientesAtendidos() {
+		return qtdClientesAtendidos;
+	}
+	public void setQtdClientesAtendidos(int qtdClientesAtendidos) {
+		this.qtdClientesAtendidos = qtdClientesAtendidos;
+	}
+	public String getEnderecoLojaRua() {
+		return enderecoLojaRua;
+	}
+	public void setEnderecoLojaRua(String enderecoLojaRua) {
+		this.enderecoLojaRua = enderecoLojaRua;
+	}
+	public String getEderecoLojaBairro() {
+		return ederecoLojaBairro;
+	}
+	public void setEderecoLojaBairro(String ederecoLojaBairro) {
+		this.ederecoLojaBairro = ederecoLojaBairro;
+	}
+	public String getEnderecoLojaCep() {
+		return enderecoLojaCep;
+	}
+	public void setEnderecoLojaCep(String enderecoLojaCep) {
+		this.enderecoLojaCep = enderecoLojaCep;
+	}
+	public String getEnderecoLojaNumero() {
+		return enderecoLojaNumero;
+	}
+	public void setEnderecoLojaNumero(String enderecoLojaNumero) {
+		this.enderecoLojaNumero = enderecoLojaNumero;
+	}
+	public String getEnderecoLojaComplemento() {
+		return enderecoLojaComplemento;
+	}
+	public void setEnderecoLojaComplemento(String enderecoLojaComplemento) {
+		this.enderecoLojaComplemento = enderecoLojaComplemento;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public void setTelefone(String telefone, String ddd) {
+		this.telefone = ddd+"-"+telefone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getWhatapp() {
+		return whatapp;
+	}
+	public void setWhatapp(String whatapp) {
+		this.whatapp = whatapp;
+	}
+	public String getNomeLoja() {
+		return nomeLoja;
+	}
+	public void setNomeLoja(String nomeLoja) {
+		this.nomeLoja = nomeLoja;
+	}
+	
+	
+	
 }

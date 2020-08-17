a980984e01e99488f8e6e70bb07270b8df4bc01d
Ricardo <ricarducorrea@gmail.com>
2020-08-17 19:51:25 -0300

    O início
    Arquivos iniciais do projeto loja

---
+++ src/br/unifacol/loja/Cliente.java
@@ -0,0 +1,24 @@
+package br.unifacol.loja;
+
+import java.util.Date;
+
+public class Cliente {
+	public String login;
+	public String senha;
+	public String nome;
+	public String cpf;
+	public int codigo;
+	public Date dataCadastro;
+	public Date dataUltimaCompra;
+	public Date dataNascimento;
+	public boolean situacao;	 
+	public String enderecoRua;
+	public String ederecoBairro;
+	public String enderecoCep;
+	public String enderecoNumero;
+	public String enderecoComplemento;
+	public String telefone;
+	public String email;
+	public String whatapp;
+ 
+}

8eeb4bd9abc33867fe887329534bca31c7055c71
Ricardo <Ricardo@DESKTOP-Q3I09MN>
2020-08-17 21:47:23 -0300

    add linha de impressão

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,7 @@ public class Main {
 		Vendedor v3 = new Vendedor("a",  f.parse("12/01/1995"), "a", "a", "a", "a", "a", "a", "a", "a", "a",f.parse("12/01/1995"),f.parse("12/01/1995"),false,1, 0, 1, 5, 
 				"a", "a", "a", "a", "a", "a", "a", "a", "a");
 		
+		System.out.println("Vendedor cadastrado com sucesso!!");
 		 
 		
 		
